28403ce64a5ee1ff64ed7ce4a5ee9147215b7cc8
PlayWay <dim.art2011@yandex.ru>
2022-03-31 10:12:35 +0300

    Добавил parser тесты в цикл

--- src/lesson2/parser.test.ts
+++ src/lesson2/parser.test.ts
@@ -1,58 +1,26 @@
 import {parser} from "./parser";
 
-test("parser: 1 + 32", () => {
-  expect(parser("1 + 32")).toEqual([1, "+", 32]);
+test.each([
+  {expression: "1 + + 33 - 2", expected: TypeError("Unexpected string")},
+  {expression: "1 ! 33 - 2", expected: TypeError("Unexpected string")},
+  {expression: "3*", expected: TypeError("Unexpected string")},
+  {expression: "3!! + 5 / 2", expected: TypeError("Unexpected string")},
+  {expression: "5^ - 7", expected: TypeError("Unexpected string")},
+  {expression: "5^2 - css", expected: TypeError("Unexpected string")},
+  {expression: "1 - sin(TEST)", expected: TypeError("Unexpected string")},
+])('parser: $expression', ({expression, expected}) => {
+  expect(() => parser(expression)).toThrow(TypeError("Unexpected string"));
+})
+
+test.each([
+  {expression: "1 + 32", expected: [1, "+", 32]},
+  {expression: "11 + 3 * 22", expected: [11, "+", 3, "*", 22]},
+  {expression: "1 + 32 - 2 + 2", expected: [1, "+", 32, "-", 2, "+", 2]},
+  {expression: "3^2 * 10 + 4!", expected: [9, '*', 10, '+', 24]},
+  {expression: "cos(10) - 3** * 10", expected: [-0.8390715290764524, '-', 9, '*', 10]},
+  {expression: "fib(10) - 7 / 5!", expected: [55, '-', 7, '/', 120]}
+])('parser: $expression', ({expression, expected}) => {
+  expect(parser(expression)).toStrictEqual(expected);
 });
 
-test("parser: 11 + 3 * 22", () => {
-  expect(parser("11 + 3 * 22")).toEqual([11, "+", 3, "*", 22]);
-});
-
-test("parser: 1 + 32 - 2 + 2", () => {
-  expect(parser("1 + 32 - 2 + 2")).toEqual([1, "+", 32, "-", 2, "+", 2]);
-});
-
-test("parser: 3^2 * 10 + 4!", () => {
-  expect(parser("3^2 * 10 + 4!")).toEqual([ 9, '*', 10, '+', 24 ]);
-});
-
-test("parser: cos(10) - 3** * 10", () => {
-  expect(parser("cos(10) - 3** * 10")).toEqual([-0.8390715290764524, '-', 9, '*', 10]);
-});
-
-test("parser: fib(10) - 7 / 5!", () => {
-  expect(parser("fib(10) - 7 / 5!")).toEqual([ 55, '-', 7, '/', 120 ]);
-});
-
-test("parser: 1 + + 33 - 2", () => {
-  expect(() => parser("1 + + 33 - 2")).toThrow(TypeError("Unexpected string"));
-});
-
-test("parser: 1 ! 33 - 2", () => {
-  expect(() => parser("1 ! 33 - 2")).toThrow(TypeError("Unexpected string"));
-});
-
-test("parser: 3*", () => {
-  expect(() => parser("3*")).toThrow(TypeError("Unexpected string"));
-});
-
-test("parser: 3!! + 5 / 2", () => {
-  expect(() => parser("3!! + 5 / 2")).toThrow(TypeError("Unexpected string"));
-});
-
-test("parser: 5^ - 7", () => {
-  expect(() => parser("5^ - 7")).toThrow(TypeError("Unexpected string"));
-});
-
-test("parser: 5^ - 7", () => {
-  expect(() => parser("5^ - 7")).toThrow(TypeError("Unexpected string"));
-});
-
-test("parser: 5^2 - css", () => {
-  expect(() => parser("5^2 - css")).toThrow(TypeError("Unexpected string"));
-});
-
-test("parser: 1 - sin(TEST)", () => {
-  expect(() => parser("1 - sin(TEST)")).toThrow(TypeError("Unexpected string"));
-});
 
