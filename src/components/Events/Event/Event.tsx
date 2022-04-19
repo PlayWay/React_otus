f0b267c2525ae04f6b23361a6716ae18a8aa599c
PlayWay <dim.art2011@yandex.ru>
2022-04-19 22:44:05 +0300

    run lint fix(test)

--- src/components/Events/Event/Event.tsx
+++ src/components/Events/Event/Event.tsx
@@ -1,15 +1,20 @@
-import React, {FC} from 'react';
-import s from './Event.module.scss';
-import {EventButton} from "../../../types";
+import React, { FC } from "react";
+import s from "./Event.module.scss";
+import { EventButton } from "../../../types";
 
-const Event: FC<EventButton> = ({src = '', name = '', color = ''}) => {
-
+const Event: FC<EventButton> = ({ src = "", name = "", color = "" }) => {
   return (
     <div className={s.event}>
-      <div style={{backgroundColor: color}} className={s.img} data-testid="event-img-wrap">
-        <img src={src} alt={name} data-testid="event-img"/>
+      <div
+        style={{ backgroundColor: color }}
+        className={s.img}
+        data-testid="event-img-wrap"
+      >
+        <img src={src} alt={name} data-testid="event-img" />
       </div>
-      <span className={s.text} data-testid="event-name">{name}</span>
+      <span className={s.text} data-testid="event-name">
+        {name}
+      </span>
     </div>
   );
 };
--- src/components/Events/Events.test.tsx
+++ src/components/Events/Events.test.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {render,screen} from "@testing-library/react";
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import {Events} from "./Events";
+import { Events } from "./Events";
 import userEvent from "@testing-library/user-event";
 
 const events = [
@@ -9,33 +9,33 @@ const events = [
     name: "Кормление",
     src: "images/child_care.svg",
     color: "red",
-    type: 1
+    type: 1,
   },
   {
     name: "Сон",
     src: "images/moon-fill.svg",
     color: "green",
-    type: 2
+    type: 2,
   },
   {
     name: "Прогулка",
     src: "images/moon-fill.svg",
     color: "green",
-    type: 3
-  }
-]
+    type: 3,
+  },
+];
 
-describe('Events', () => {
-  it('markup event list', () => {
-    const onEventClick = jest.fn()
-    render(<Events events={events} onEventClick={onEventClick}/>)
-    expect(screen.getAllByRole("button").length).toBe(3)
+describe("Events", () => {
+  it("markup event list", () => {
+    const onEventClick = jest.fn();
+    render(<Events events={events} onEventClick={onEventClick} />);
+    expect(screen.getAllByRole("button").length).toBe(3);
   });
-  it('call button click func', async () => {
-    const onEventClick = jest.fn()
-    render(<Events events={events} onEventClick={onEventClick}/>)
-    const buttons = screen.getAllByRole("button")
-    await userEvent.click(buttons[1])
-    expect(onEventClick).toHaveBeenCalledWith(2)
+  it("call button click func", async () => {
+    const onEventClick = jest.fn();
+    render(<Events events={events} onEventClick={onEventClick} />);
+    const buttons = screen.getAllByRole("button");
+    await userEvent.click(buttons[1]);
+    expect(onEventClick).toHaveBeenCalledWith(2);
   });
 });
