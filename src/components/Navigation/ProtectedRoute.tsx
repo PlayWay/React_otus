6a98ab90e8f8a9534dc75fb86fa02b5266175b3a
PlayWay <dim.art2011@yandex.ru>
2022-06-14 22:25:19 +0300

    fix eslint error

--- src/components/Navigation/ProtectedRoute.tsx
+++ src/components/Navigation/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren, useContext } from "react";
 import { AuthProviderContext } from "../Auth/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
-export const ProtectedRoute: React.FC<PropsWithChildren<unknown>> = ({
+export const ProtectedRoute: React.FC<PropsWithChildren<any>> = ({
   children,
 }) => {
   const { user } = useContext(AuthProviderContext);
